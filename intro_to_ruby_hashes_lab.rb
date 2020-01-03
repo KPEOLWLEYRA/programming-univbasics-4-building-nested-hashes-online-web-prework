bca58f04eae3911bb7119f6295859202925f1591
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:29:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
   	  :reading_railroad => {}, 
   	  :pennsylvania_railroad => {}, 
   	  :b_and_o_railroad => {}, 
-  	  :ky => {}
+  	  :shortline_railroad => {}
   	}
 	monopoly_with_third_tier
   
