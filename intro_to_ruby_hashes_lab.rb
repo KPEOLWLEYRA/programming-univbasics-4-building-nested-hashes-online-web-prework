efe69b3a24fde07437ab83495257ca818547d818
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 19:20:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,4 +49,20 @@ def monopoly_with_fourth_tier
   [monopoly_with_fourth_tier][:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$100"
   [monopoly_with_fourth_tier][:railroads][:names][:shortline_railroad]["mortgage_value"] = "$100"
   monopoly_with_fourth_tier
+  
+  monopoly_with_third_tier = monopoly_with_second_tier
+  	monopoly_with_third_tier[:railroads][:rent_in_dollars] = {
+  	  :one_piece_owned => 25, 
+  	  :two_pieces_owned => 50, 
+  	  :three_pieces_owned => 100, 
+  	  :four_pieces_owned => 200
+  	} 
+  	monopoly_with_third_tier[:railroads][:names] = {
+  	  :reading_railroad => {"mortgage_value" => "$100"}, 
+  	  :pennsylvania_railroad => {"mortgage_value" => "$100"}, 
+  	  :b_and_o_railroad => {"mortgage_value" => "$100"}, 
+  	  :shortline_railroad => {"mortgage_value" => "$100"}
+  	}
+	monopoly_with_third_tier
+  
 end
