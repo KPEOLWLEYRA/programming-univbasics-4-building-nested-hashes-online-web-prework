2af4946885729e68ea01936c0f7d916ed60a5797
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:34:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,19 +22,19 @@ def monopoly_with_third_tier
   # complexity.
     monopoly = {:railroads => {} }
   	monopoly[:railroads][:pieces] = 4
-  	monopoly_with_third_tier[:railroads][:rent_in_dollars] = {
+  	monopoly[:railroads][:rent_in_dollars] = {
   	  :one_piece_owned => 25, 
   	  :two_pieces_owned => 50, 
   	  :three_pieces_owned => 100, 
   	  :four_pieces_owned => 200
   	} 
-  	monopoly_with_third_tier[:railroads][:names] = {
+  	monopoly[:railroads][:names] = {
   	  :reading_railroad => {}, 
   	  :pennsylvania_railroad => {}, 
   	  :b_and_o_railroad => {}, 
   	  :shortline_railroad => {}
   	}
-	monopoly_with_third_tier
+	monopoly
   
 end
 
@@ -43,4 +43,19 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+     monopoly = {:railroads => {} }
+  	monopoly[:railroads][:pieces] = 4
+  	monopoly[:railroads][:rent_in_dollars] = {
+  	  :one_piece_owned => 25, 
+  	  :two_pieces_owned => 50, 
+  	  :three_pieces_owned => 100, 
+  	  :four_pieces_owned => 200
+  	} 
+  	monopoly[:railroads][:names] = {
+  	  :reading_railroad => {}, 
+  	  :pennsylvania_railroad => {}, 
+  	  :b_and_o_railroad => {}, 
+  	  :shortline_railroad => {}
+  	}
+	monopoly
 end
