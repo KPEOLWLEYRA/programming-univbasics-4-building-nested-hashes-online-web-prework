63e30dcc1b58f8d587606b64ba9abe1dc0f0a393
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:37:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	monopoly = {:railroads => {} }
-	monopoly
+	base_hash = {:railroads => {} }
+	base_hash
 end
 
 def monopoly_with_second_tier
@@ -10,9 +10,9 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  	monopoly = {:railroads => {} }
-  	monopoly[:railroads][:pieces] = 4
-	monopoly
+  	monopoly_with_second_tier = base_hash
+  	monopoly_with_second_tier[:railroads][:pieces] = 4
+	monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
@@ -20,21 +20,20 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-    monopoly = {:railroads => {} }
-  	monopoly[:railroads][:pieces] = 4
-  	monopoly[:railroads][:rent_in_dollars] = {
+    monopoly_with_third_tier = monopoly_with_second_tier
+  	monopoly_with_third_tier[:railroads][:rent_in_dollars] = {
   	  :one_piece_owned => 25, 
   	  :two_pieces_owned => 50, 
   	  :three_pieces_owned => 100, 
   	  :four_pieces_owned => 200
   	} 
-  	monopoly[:railroads][:names] = {
+  	monopoly_with_third_tier[:railroads][:names] = {
   	  :reading_railroad => {}, 
   	  :pennsylvania_railroad => {}, 
   	  :b_and_o_railroad => {}, 
   	  :shortline_railroad => {}
   	}
-	monopoly
+	monopoly_with_third_tier
   
 end
 
@@ -43,19 +42,6 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-     monopoly = {:railroads => {} }
-  	monopoly[:railroads][:pieces] = 4
-  	monopoly[:railroads][:rent_in_dollars] = {
-  	  :one_piece_owned => 25, 
-  	  :two_pieces_owned => 50, 
-  	  :three_pieces_owned => 100, 
-  	  :four_pieces_owned => 200
-  	} 
-  	monopoly[:railroads][:names] = {
-  	  :reading_railroad => {}, 
-  	  :pennsylvania_railroad => {}, 
-  	  :b_and_o_railroad => {}, 
-  	  :shortline_railroad => {}
-  	}
-	monopoly
+  monopoly_with_fourth_tier = monopoly_with_third_tier
+  [monopoly_with_fourth_tier][:railroads][:names]
 end
