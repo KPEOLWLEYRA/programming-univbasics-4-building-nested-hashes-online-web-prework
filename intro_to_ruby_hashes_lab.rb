b97d30a78219d76f9475edf55f1e25a50758f740
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:20:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # complexity.
     monopoly_with_third_tier = monopoly_with_second_tier
   	monopoly_with_third_tier[:railroads][:rent_in_dollars] = {
-  	  :names => "1", 
+  	  [:names], 
   	  :key2 => "2", 
   	  :key3 => "3", 
   	  :key4 => "4"
