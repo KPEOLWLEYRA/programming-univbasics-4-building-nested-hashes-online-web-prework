b39b2acacf77fdae44437f2ce6a03a18609abd9e
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:14:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
     monopoly_with_third_tier = monopoly_with_second_tier
-  	monopoly_with_third_tier[:railroads][:rent_in_dollars] = 6
+  	monopoly_with_third_tier[:railroads][:rent_in_dollars] = {1, 2, 3, 4} 
 	monopoly_with_third_tier
   
 end
