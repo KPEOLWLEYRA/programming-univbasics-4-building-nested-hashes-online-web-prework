bfae27f9d6858b0a676c73df14c7adb5cae8c2b1
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:12:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,6 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  complexity.
     monopoly_with_third_tier = monopoly_with_second_tier
   	monopoly_with_third_tier[:railroads][:rent_in_dollars]
 	monopoly_with_third_tier
