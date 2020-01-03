b788622ab0245e2cd949ab5158c7a403e89ae8ea
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 18:32:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,9 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  	monopoly_with_second_tier = base_hash
+  	monopoly = {:railroads => {}}
   	monopoly_with_second_tier[:railroads][:pieces] = 4
-	monopoly_with_second_tier
+	monopoly
 end
 
 def monopoly_with_third_tier
