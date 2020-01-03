364cab05d48de3a665a4ae0c8b0ad63a4f1d0dde
Kelly Powers <kpowers.nj@gmail.com>
2020-01-03 19:21:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly_with_fourth_tier = monopoly_with_third_tier
+  #monopoly_with_fourth_tier = monopoly_with_third_tier
   [monopoly_with_fourth_tier][:railroads][:names][:reading_railroad] = 
       {"mortgage_value" => "$100"}
   [monopoly_with_fourth_tier][:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$100"
@@ -61,7 +61,7 @@ def monopoly_with_fourth_tier
   	  :reading_railroad => {"mortgage_value" => "$100"}, 
   	  :pennsylvania_railroad => {"mortgage_value" => "$100"}, 
   	  :b_and_o_railroad => {"mortgage_value" => "$100"}, 
-  	  :shortline_railroad => {"mortgage_value" => "$100"}
+  	  :shortline_railroad => {"mortgage_value" => "$100"}, 
   	}
 	monopoly_with_third_tier
   
